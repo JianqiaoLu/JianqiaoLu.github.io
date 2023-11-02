a466b8ec0738399ed03e4191beb6fae1769b1b94
lujianqiao <tjlujianqiao@gmail.com>
2023-11-02 19:52:08 +0800

    first commit

---
+++ Zhiyi Huang's Homepage_files/author-list.js
@@ -0,0 +1,387 @@
+var authorList = {
+
+    Azar : {
+        name : "Yossi Azar",
+        web : "http://www.cs.tau.ac.il/~azar/",
+    },
+
+    Bei : {
+        name : "Xiaohui Bei",
+        web : "https://www.ntu.edu.sg/home/xhbei/",
+    },
+
+    Bubeck : {
+        name : "S&#233;bastian Bubeck",
+        web : "http://sbubeck.com/",
+    },
+
+    Buchbinder : {
+        name : "Niv Buchbinder",
+        web : "http://www.tau.ac.il/~nivb/",
+    },
+
+    Cai : {
+        name : "Yang Cai",
+        web : "http://www.cs.yale.edu/homes/cai/",
+    },
+
+    Chan : {
+        name : "T-H. Hubert Chan",
+        web : "http://www.cs.hku.hk/~hubert/",
+    },
+
+    Chakrabarty : {
+        name : "Deeparnab Chakrabarty",
+        web : "https://www.cs.dartmouth.edu/~deepc/",
+    },
+
+    Chakraborty : {
+        name : "Tanmoy Chakraborty",
+        web : "#"
+    },
+
+    Chen : {
+        name : "Shiteng Chen",
+        web : "https://iiis.tsinghua.edu.cn/shitengchen/",
+    },
+
+    ChenS : {
+        name : "Shahar Chen",
+        web : "#",
+    },
+
+    ChenZ : {
+        name : "Ziyun Chen",
+        web : "#",
+    },
+
+    Cohen : {
+        name : "Reuven Cohen",
+        web : "http://u.math.biu.ac.il/~reuven/",
+    },
+
+    Cummings : {
+        name : "Rachel Cummings",
+        web : "https://pwp.gatech.edu/rachel-cummings/"
+    },
+
+    Devanur : {
+        name : "Nikhil R. Devanur",
+        web : "https://www.nikhildevanur.com/"
+    },
+
+    Du : {
+        name : "Bingqian Du",
+        web : "#"
+    },
+
+    Dudik : {
+        name : "Miro Dud&#237;k",
+        web : "http://www.cs.princeton.edu/~mdudik/"
+    },
+
+    Fahrbach : {
+        name : "Matthew Fahrbach",
+        web : "https://research.google/people/106910/",
+    },
+
+    GaoRuiquan : {
+        name : "Ruiquan Gao",
+        web : "https://rqgao0422.github.io/"
+    },
+
+    Guha : {
+        name : "Sudipto Guha",
+        web : "http://www.cis.upenn.edu/~sudipto/"
+    },
+
+    Guo : {
+        name : "Chenghao Guo",
+        web : "#"
+    },
+
+    Gupta : {
+        name : "Anumpam Gupta",
+        web : "http://www.cs.cmu.edu/~anupamg/"
+    },
+
+    HeZhongtian : {
+        name : "Zhongtian He",
+        web : "#"
+    },
+
+    Hosseini : {
+        name : "Hadi Hosseini",
+        web : "https://faculty.ist.psu.edu/hadi/"
+    },
+
+    HuYihang: {
+        name : "Yihang Hu",
+        web : "#"
+    },
+
+    Hsu : {
+        name : "Justin Hsu",
+        web : "https://justinh.su/",
+    },
+
+    Igarashi : {
+        name : "Ayumi Igarashi",
+        web : "https://sites.google.com/site/eigarashayumi/home",
+    },
+
+    Jiang : {
+        name : "Shaofeng Jiang",
+        web : "http://www.wisdom.weizmann.ac.il/~shaofeng/"
+    },
+
+    JiangHanrui : {
+        name : "Hanrui Jiang",
+        web : "#",
+    },
+
+    Kang : {
+        name : "Ning Kang",
+        web : "http://www.cs.hku.hk/~nkang/"
+    },
+
+    Kannan : {
+        name : "Sampath Kannan",
+        web : "http://www.cis.upenn.edu/~kannan/"
+    },
+
+    Khanna : {
+        name : "Sanjeev Khanna",
+        web : "http://www.cis.upenn.edu/~sanjeev/"
+    },
+
+    Kim : {
+        name : "Anthony Kim",
+        web : "http://web.stanford.edu/~tonyekim/"
+    },
+
+    Korula : {
+        name : "Nitish Korula",
+        web : "http://research.google.com/pubs/NitishKorula.html"
+    },
+
+    Panigrahi : {
+        name : "Debmalya Panigrahi",
+        web : "http://www.cs.duke.edu/~debmalya/"
+    },
+
+    Pennock : {
+        name : "David M. Pennock",
+        web : "http://dpennock.com/",
+    },
+
+    Lau : {
+        name : "Francis C.M. Lau",
+        web : "http://www.cs.hku.hk/~fcmlau/",
+    },
+
+    Li : {
+        name : "Zongpeng Li",
+        web : "http://pages.cpsc.ucalgary.ca/~zongpeng/",
+    },
+
+    LiMinming : {
+        name : "Minming Li",
+        web : "https://www.cs.cityu.edu.hk/~minmli/",
+    },
+
+    Liu : {
+        name : "Jinyan Liu",
+        web : "https://cs.bit.edu.cn/szdw/jsml/fjs/ljy/index.htm",
+    },
+
+    Majdi : {
+        name : "Dorsa Majdi",
+        web : "#",
+    },
+
+    Malec : {
+        name : "David Malec",
+        web : "http://pages.cs.wisc.edu/~dmalec/"
+    },
+
+    Mansour : {
+        name : "Yishay Mansour",
+        web : "http://www.math.tau.ac.il/~mansour/"
+    },
+
+    Mirrokni : {
+        name : "Vahab Mirrokni",
+        web : "http://people.csail.mit.edu/mirrokni/"
+    },
+
+    Nagarajan : {
+        name : "Viswanath Nagarajan",
+        web : "http://viswa.engin.umich.edu/"
+    },
+
+    Naor : {
+        name : "Joseph (Seffi) Naor",
+        web : "http://www.cs.technion.ac.il/~naor/"
+    },
+
+    Niazadeh : {
+        name : "Rad Niazadeh",
+        web : "https://radniazadeh.github.io/"
+    },
+
+    NieZipei : {
+        name : "Zipei Nie",
+        web : "#"
+    },
+
+    Peng : {
+        name : "Binghui Peng",
+        web : "http://www.cs.columbia.edu/~binghuip/"
+    },
+
+    Psomas : {
+        name : "Christos-Alexandros Psomas",
+        web : "https://www.alexpsomas.com/"
+    },
+
+    Roth : {
+        name : "Aaron Roth",
+        web : "http://www.cis.upenn.edu/~aaroth/"
+    },
+
+    Roughgarden : {
+        name : "Tim Roughgarden",
+        web : "http://timroughgarden.org/"
+    },
+
+    Shah : {
+        name : "Nisarg Shah",
+        web : "https://www.cs.toronto.edu/~nisarg/"
+    },
+
+    ShenAocheng : {
+        name : "Aocheng Shen",
+        web : "#",
+    },
+
+    ShenYiheng : {
+        name : "Yiheng Shen",
+        web : "https://sites.google.com/view/yiheng-shen/",
+    },
+
+    Shu : {
+        name : "Xinkai Shu",
+        web : "https://shuxk.github.io/",
+    },
+
+    SongJunkai : {
+        name : "Junkai Song",
+        web : "#",
+    },
+
+    Tang : {
+        name : "Zhihao Gavin Tang",
+        web : "http://itcs.shufe.edu.cn/~zhtang/",
+    },
+
+    Tao : {
+        name : "Runzhou Tao",
+        web : "http://taorunz.me/",
+    },
+
+    Wang : {
+        name : "Lei Wang",
+        web : "#",
+    },
+    
+    WangX : {
+        name : "Xiangning Wang",
+        web : "#",
+    },
+
+    WeiTianze : {
+        name : "Tianze Wei",
+        web :"#",
+    },
+
+    WuChuan : {
+        name : "Chuan Wu",
+        web : "http://www.cs.hku.hk/~cwu/"
+    },
+
+    Wu : {
+        name : "Zhiwei Steven Wu",
+        web : "https://zstevenwu.com/",
+    },
+
+    WuX : {
+        name : "Xiaowei Wu",
+        web : "https://sites.google.com/site/wxw0711/",
+    },
+
+    WuZhiang : {
+        name : "Zhiang Wu",
+        web : "#",
+    },
+
+    Yan : {
+        name : "Qiqi Yan",
+        web : "http://www.qiqiyan.com/",
+    },
+
+    YanShuyi : {
+        name : "Shuyi Yan",
+        web : "#"
+    },
+
+    YanZipeng : {
+        name : "Zipeng Yan",
+        web : "#",
+    },
+
+    YuanBijun : {
+        name : "Bijun Yuan",
+        web : "#"
+    },
+
+    Zadimoghaddam : {
+        name : "Morteza Zadimoghaddam",
+        web : "https://research.google/people/MortezaZadimoghaddam/",
+    },
+
+    Zhang : {
+        name : "Xiaoxi Zhang",
+        web : "https://www.andrew.cmu.edu/user/xiaoxiz2/",
+    },
+    
+    ZhangQiankun : {
+        name : "Qiankun Zhang",
+        web : "https://qiankuntcs.github.io/"
+    },
+
+    ZhangXinzhi : {
+        name : "Xinzhi Zhang",
+        web : "#"
+    },
+
+    ZhangY : {
+        name : "Yuhao Zhang",
+        web : "http://www.zyhwtc.com/",
+    },
+
+    ZhongYan : {
+        name : "Yan Zhong",
+        web : "#",
+    },
+
+    Zhou : {
+        name : "Yuan Zhou",
+        web : "https://yuanz.web.illinois.edu/",
+    },
+
+    Zhu : {
+        name : "Xue Zhu",
+        web : "#"
+    },
+};
